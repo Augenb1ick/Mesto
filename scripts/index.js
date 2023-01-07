8533f5d462847372679be5a61a22ffef19971b45
Savoskin Nikita <augenblick14@yandex.ru>
2023-01-07 14:00:12 +0400

    ready

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,12 @@
 let likeButtons = document.querySelectorAll('.content__like-image');
-let PopUpContainer = document.querySelector('.pop-up-container');
 let editButton = document.querySelector('.profile__info-edit-button');
 let theName = document.querySelector('.profile__info-heading');
 let profession = document.querySelector('.profile__info-subheading');
-let SubmitButton;
-let newName;
-let newProfession;
-let closeButton;
-let popupForm;
+let SubmitButton = document.querySelector('.pop-up__submit-btn');
+let newName = document.querySelector('.pop-up__text_type_name');
+let newProfession = document.querySelector('.pop-up__text_type_profession');
+let closeButton = document.querySelector('.pop-up__close-btn');
+let popup = document.querySelector('.pop-up');
 
 likeButtons.forEach(function(likeButton) {
     likeButton.onclick = function(event) {
@@ -16,40 +15,26 @@ likeButtons.forEach(function(likeButton) {
 })
 
 function PopUp() {
-    PopUpContainer.innerHTML = `
-        <form class="pop-up" tabindex="2">
-            <div class="pop-up__content">
-                <h2 class="pop-up__heading">Редактировать профиль</h2>
-                <input type="text" class="pop-up__text pop-up__text_type_name" value="${theName.innerHTML}">
-                <input type="text" class="pop-up__text pop-up__text_type_profession" value="${profession.innerHTML}">
-                <button class="pop-up__submit-btn" type="submit">Сохранить</button>
-            </div>
-            <button class="pop-up__close-btn" type="button" tabindex="3"></button>
-        </form>
-    `;
-    PopUpContainer.classList.add('pop-up-bg');
-
-    popupForm = document.querySelector('.pop-up');
-    SubmitButton = document.querySelector('.pop-up__submit-btn');
-    newName = document.querySelector('.pop-up__text_type_name');
-    newProfession = document.querySelector('.pop-up__text_type_profession');
-    closeButton = document.querySelector('.pop-up__close-btn');
+    popup.classList.add('pop-up_opened')
+    
+    newName.value = theName.innerHTML;
+    newProfession.value = profession.innerHTML;
 
     closeButton.addEventListener('click', PopUpClose);
     SubmitButton.addEventListener('click', SubmitClick);
-    popupForm.addEventListener('submit', handleFormSubmit);
+    popup.addEventListener('submit', handleFormSubmit);
 }
 
 editButton.addEventListener('click', PopUp);
 
 function PopUpClose() {
-    PopUpContainer.innerHTML = '';
-    PopUpContainer.classList.remove('pop-up-bg');
+    popup.classList.remove('pop-up_opened');
 }
 
 function SubmitClick() {
     theName.innerHTML = newName.value;
     profession.innerHTML = newProfession.value;
+
     PopUpClose();
 }
 
@@ -58,8 +43,8 @@ function handleFormSubmit(evt) {
 
     theName.innerHTML = newName.value;
     profession.innerHTML = newProfession.value;
+
     PopUpClose();
-    
 }
 
 
