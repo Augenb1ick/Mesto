35141d3c4848cea0b036863fc7fd448d5598370f
Savoskin Nikita <augenblick14@yandex.ru>
2023-01-08 15:55:56 +0400

    ready-for-the-2nd-review

--- scripts/index.js
+++ scripts/index.js
@@ -1,51 +1,45 @@
-let likeButtons = document.querySelectorAll('.content__like-image');
 let editButton = document.querySelector('.profile__info-edit-button');
 let theName = document.querySelector('.profile__info-heading');
-let profession = document.querySelector('.profile__info-subheading');
-let SubmitButton = document.querySelector('.pop-up__submit-btn');
-let newName = document.querySelector('.pop-up__text_type_name');
-let newProfession = document.querySelector('.pop-up__text_type_profession');
-let closeButton = document.querySelector('.pop-up__close-btn');
-let popup = document.querySelector('.pop-up');
+let theProfession = document.querySelector('.profile__info-subheading');
+let newName = document.querySelector('.popup__text_type_name');
+let newProfession = document.querySelector('.popup__text_type_profession');
+let closeButton = document.querySelector('.popup__close-btn');
+let popup = document.querySelector('.popup');
+let popupForm = document.querySelector('.popup__content')
 
-likeButtons.forEach(function(likeButton) {
-    likeButton.onclick = function(event) {
-        event.target.classList.toggle('content__like-image_type_active');
-    }
-})
 
-function PopUp() {
-    popup.classList.add('pop-up_opened')
+function popUp() {
+    popup.classList.add('popup_opened')
     
-    newName.value = theName.innerHTML;
-    newProfession.value = profession.innerHTML;
-
-    closeButton.addEventListener('click', PopUpClose);
-    SubmitButton.addEventListener('click', SubmitClick);
-    popup.addEventListener('submit', handleFormSubmit);
+    newName.value = theName.textContent;
+    newProfession.value = theProfession.textContent;
 }
 
-editButton.addEventListener('click', PopUp);
-
-function PopUpClose() {
-    popup.classList.remove('pop-up_opened');
-}
-
-function SubmitClick() {
-    theName.innerHTML = newName.value;
-    profession.innerHTML = newProfession.value;
-
-    PopUpClose();
+function popUpClose() {
+    popup.classList.remove('popup_opened');
 }
 
 function handleFormSubmit(evt) {
     evt.preventDefault();
 
-    theName.innerHTML = newName.value;
-    profession.innerHTML = newProfession.value;
+    theName.textContent = newName.value;
+    theProfession.textContent = newProfession.value;
 
-    PopUpClose();
+    popUpClose();
 }
+
+editButton.addEventListener('click', popUp);
+closeButton.addEventListener('click', popUpClose);
+popupForm.addEventListener('submit', handleFormSubmit);
+
+
+// функция активации кнопки лайка, которую видимо нужно реализовать в следующем проекте. (хоть и в фигме есть такое состояние кнопки)
+// let likeButtons = document.querySelectorAll('.content__like-button')
+// likeButtons.forEach(function(likeButton) {
+//     likeButton.onclick = function(event) {
+//         event.target.classList.toggle('.content__like-button_type_active');
+//     }
+// })
 
 
 
